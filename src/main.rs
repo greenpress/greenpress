a0e954194dcfba4b75a2721776b81ae078c6db40
Ben Ukhanov <ben1zuk321@gmail.com>
2020-12-13 22:44:55 +0200

    feat(httpauth) added comments

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ async fn validator(req: ServiceRequest, auth: BearerAuth) -> Result<ServiceReque
     // https://github.com/actix/actix-web/issues/1818
     let (http_req, payload) = req.into_parts();
     let cookie = http_req.cookie("token=");
-    // TODO: Don't use unwrap() because panics
+    // TODO: Don't use unwrap() because panics?
     let mut req = ServiceRequest::from_parts(http_req, payload).ok().unwrap();
 
     if auth.token().trim().is_empty() || cookie.is_some() {
@@ -31,15 +31,17 @@ async fn validator(req: ServiceRequest, auth: BearerAuth) -> Result<ServiceReque
     let auth_req = Client::new()
         .get(me_url)
         .bearer_auth(auth.token())
-        .cookie(cookie.unwrap()) // unwrap() should never panic
+        .cookie(cookie.unwrap()) // NOTE: unwrap() should never panic
         .content_type("application/json");
     let mut auth_res = auth_req.send().await?;
     let body = auth_res.body().await?;
+    // TODO: What if there is no json response? -> If there is no json, will we send an empty header value?
+    // TODO: What if there is error as a (json?) response? -> Handle here or pass to service?
     let user = str::from_utf8(body.as_ref())?;
 
     req.headers_mut().insert(
         HeaderName::from_static(USER_HEADER),
-        HeaderValue::from_str(user)?,
+        HeaderValue::from_str(user)?, // TODO: No error
     );
 
     Ok(req)
