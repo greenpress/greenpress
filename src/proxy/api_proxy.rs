0dc52c6cb3403ca288cc2c0fa45d007b98f64a33
Ben Ukhanov <ben1zuk321@gmail.com>
2021-01-30 19:02:18 +0200

    docs: remove comments

--- src/proxy/api_proxy.rs
+++ src/proxy/api_proxy.rs
@@ -21,16 +21,12 @@ pub async fn forward(
     // TODO: Handle any other route (default route)
     let mut res = HttpResponse::Ok();
     let app_config = req.app_data::<web::Data<AppConfig>>().unwrap();
-    let bearer_token = get_bearer_token(&req); // req.headers.authorization
-    let cookie_token = req.cookie("token"); // req.headers.cookie.includes('token=')
+    let bearer_token = get_bearer_token(&req);
+    let cookie_token = req.cookie("token");
     let mut user: String = "".to_owned();
 
-    // if (!(req.headers.authorization || req.headers.cookie && req.headers.cookie.includes('token=')))
     if bearer_token.is_some() || cookie_token.is_some() {
-        // const meUrl = getProxyTarget(authService) + '/api/me'
         let me_url = get_me_url(&app_config);
-
-        // fetch(meUrl, headers...)
         let mut auth_res = actix_web::client::Client::new()
             .get(&me_url)
             .bearer_auth(bearer_token.unwrap_or_default())
@@ -40,22 +36,16 @@ pub async fn forward(
             .await?;
 
         let bytes = auth_res.body().await?;
-        // const setCookie = response.headers.raw()['set-cookie']
         let set_cookie = auth_res.headers().get(SET_COOKIE);
 
-        // if (setCookie)
         if set_cookie.is_some() {
-            // res.set('set-cookie', setCookie)
             res.set_header(SET_COOKIE, set_cookie.unwrap().to_owned());
         }
 
-        // if (response.status === 200)
         if auth_res.status() == StatusCode::OK {
-            // return response.text()
             return Ok(res.body(bytes));
         }
 
-        // req.user = user
         user = String::from_utf8_lossy(&bytes).to_string();
     }
 
@@ -66,7 +56,7 @@ pub async fn forward(
     let forwarded_req = get_forwarded_req(client, &req, &new_url);
     let mut proxy_res = forwarded_req
         .header(TENANT_HEADER, app_config.tenant.to_owned())
-        .header(USER_HEADER, user.to_owned()) // proxyReq.setHeader('user', req.user || '')
+        .header(USER_HEADER, user.to_owned())
         .send_body(body)
         .await
         .map_err(Error::from)?;
