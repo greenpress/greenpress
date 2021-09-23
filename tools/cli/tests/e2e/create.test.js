0eeb4953f74bc097e17265fb06ed89ba873ecb16
David Meir-Levy <davidmeirlevy@gmail.com>
2021-09-24 00:13:35 +0300

    added e2e tests for cli

---
+++ tools/cli/tests/e2e/create.test.js
@@ -0,0 +1,29 @@
+const { rmdirSync, mkdirSync, existsSync } = require('fs');
+const { execSync } = require('child_process');
+
+describe("Create command", () => {
+  beforeEach(() => {
+    try {
+      rmdirSync('tmp', { recursive: true });
+    } catch {
+      //
+    }
+    mkdirSync('tmp');
+    execSync('npm link');
+  });
+  afterAll(() => {
+    rmdirSync('tmp', { recursive: true });
+  })
+  it('should create an app', () => {
+    execSync('greenpress create my-app', { cwd: './tmp' });
+    expect(existsSync('tmp/my-app')).toBeTruthy();
+  });
+
+  it('should create an app for a user', () => {
+    execSync('greenpress create my-app2 user', { cwd: './tmp' });
+    expect(existsSync('tmp/my-app2')).toBeTruthy();
+    const remotes = execSync('git remote', { cwd: './tmp/my-app2' }).toString();
+
+    expect(remotes).toContain('gp');
+  });
+})
