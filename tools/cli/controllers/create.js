79c356beedf8804147f72c53e726a018132cc740
Kfir Zvi <zvikfir10@gmail.com>
2021-09-28 00:32:31 +0300

    Added required modifications for cli tool create command
    + runs npm install after cloning the greenpress repo.
    + checks whether pm2 is installed globally using `which`, and installs it locally if necessary.

--- tools/cli/controllers/create.js
+++ tools/cli/controllers/create.js
@@ -1,30 +1,58 @@
-const execute = require('../utils/execute');
-const { renameOrigin, setupEnvForWindows } = require('../services/create');
-const { red, green, blue } = require('../utils/colors');
-const localCompositionGuide = 'https://docs.greenpress.info/guide/local-docker-composition.html'
+const execute = require("../utils/execute");
+const {
+  renameOrigin,
+  setupEnvForWindows,
+  runNpmInstallInDirectoryAndExitIfFailed,
+  installPm2WithNpmIfMissingInDirectoryAndExitIfFailed,
+} = require("../services/create");
+const { red, green, blue } = require("../utils/colors");
+const localCompositionGuide =
+  "https://docs.greenpress.info/guide/local-docker-composition.html";
 
 module.exports = function createController({ name, mode }) {
   try {
-    execute(`git clone https://github.com/greenpress/greenpress ${name}`, 'clone greenpress');
+    execute(
+      `git clone https://github.com/greenpress/greenpress ${name}`,
+      "clone greenpress"
+    );
   } catch {
     console.log(red(`Failed to clone application!`));
-    console.log(blue('Make sure that all of Greenpress dependencies are installed - use greenpress missing'));
+    console.log(
+      blue(
+        "Make sure that all of Greenpress dependencies are installed - use greenpress missing"
+      )
+    );
     process.exit(1);
   }
 
-  if (mode === 'user') {
+  runNpmInstallInDirectoryAndExitIfFailed(name);
+
+  installPm2WithNpmIfMissingInDirectoryAndExitIfFailed(name);
+
+  if (mode === "user") {
     renameOrigin(name);
   }
 
-  if (process.platform === 'win32') {
+  if (process.platform === "win32") {
     try {
       setupEnvForWindows(name);
     } catch {
-      console.log(red(`Failed to set env correctly. To do so manually, follow our guide: ${blue(localCompositionGuide)}`));
+      console.log(
+        red(
+          `Failed to set env correctly. To do so manually, follow our guide: ${blue(
+            localCompositionGuide
+          )}`
+        )
+      );
       process.exit(1);
     }
   }
 
-  console.log(green('Done!'), `\nEnter ${blue(name)} directory, You can run the application using: ${blue('greenpress start')}`);
+  console.log(
+    green("Done!"),
+    `\nEnter ${blue(name)} directory, You can run the application using: ${blue(
+      "greenpress start"
+    )}`
+  );
   process.exit(0);
-}
+};
--- tools/cli/services/create.js
+++ tools/cli/services/create.js
@@ -1,25 +1,70 @@
-const fs = require('fs');
-const execute = require('../utils/execute');
-const { red } = require('../utils/colors');
-const { join } = require('path');
+const fs = require("fs");
+const execute = require("../utils/execute");
+const { red } = require("../utils/colors");
+const { join } = require("path");
+
+function runNpmInstallInDirectoryAndExitIfFailed(directoryName) {
+  try {
+    execute(
+      `npm install --prefix ${directoryName}`,
+      "npm install dependencies"
+    );
+  } catch {
+    console.log(red(`Failed to install dependencies using \`npm install\`.`));
+    process.exit(1);
+  }
+}
+
+function installPm2WithNpmIfMissingInDirectoryAndExitIfFailed(directoryName) {
+  if (isPm2NotExists()) {
+    installPm2WithNpmInDirectoryAndExitIfFailed(directoryName);
+  }
+}
+
+function isPm2NotExists() {
+  try {
+    execute(`which pm2`, `check if \`pm2\` is installed globally`);
+    return false;
+  } catch (err) {
+    return true;
+  }
+}
+
+function installPm2WithNpmInDirectoryAndExitIfFailed(directoryName) {
+  try {
+    execute(`npm install pm2 --prefix ${directoryName}`, "npm install pm2");
+  } catch {
+    console.log(red(`Failed to install pm2 using \`npm install\`.`));
+    process.exit(1);
+  }
+}
 
 function renameOrigin(name) {
-  execute(`git remote rename origin gp`, 'rename greenpress origin to gp', { cwd: join(process.cwd(), name) });
+  execute(`git remote rename origin gp`, "rename greenpress origin to gp", {
+    cwd: join(process.cwd(), name),
+  });
 }
 
 function setupEnvForWindows(name) {
   try {
-    const composePath = join(process.cwd(), name, 'compose');
-    execute('npm run envs', 'create env files needed to run with docker-compose', { cwd: composePath });
+    const composePath = join(process.cwd(), name, "compose");
+    execute(
+      "npm run envs",
+      "create env files needed to run with docker-compose",
+      { cwd: composePath }
+    );
 
-    let envContent = '';
-    const envFilePath = join(composePath, '.env');
+    let envContent = "";
+    const envFilePath = join(composePath, ".env");
     const envFile = fs.readFileSync(envFilePath).toString();
 
-    envFile.split('\n').forEach(element => {
-      if (element.includes('MONGODB_VOLUME')) {
-        const volumeSpecs = element.split('=');
-        envContent += `${volumeSpecs[0]}=${volumeSpecs[1].replace('/', '\\')}\n`
+    envFile.split("\n").forEach((element) => {
+      if (element.includes("MONGODB_VOLUME")) {
+        const volumeSpecs = element.split("=");
+        envContent += `${volumeSpecs[0]}=${volumeSpecs[1].replace(
+          "/",
+          "\\"
+        )}\n`;
       } else {
         envContent += `${element}\n`;
       }
@@ -28,7 +73,9 @@ function setupEnvForWindows(name) {
     fs.truncateSync(envFilePath, 0);
     fs.writeFileSync(envFilePath, envContent);
   } catch (e) {
-    console.log(red(`An error occurred while setting env correctly. Error: ${e.message}`));
+    console.log(
+      red(`An error occurred while setting env correctly. Error: ${e.message}`)
+    );
     throw e;
   }
 
@@ -37,5 +84,7 @@ function setupEnvForWindows(name) {
 
 module.exports = {
   renameOrigin,
-  setupEnvForWindows
-}
+  setupEnvForWindows,
+  runNpmInstallInDirectoryAndExitIfFailed,
+  installPm2WithNpmIfMissingInDirectoryAndExitIfFailed,
+};
