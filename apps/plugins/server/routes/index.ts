828a0b8c55bbaae5139df073c6b9dd8250f43110
David Meir-Levy <davidmeirlevy@gmail.com>
2021-10-07 14:33:56 +0300

    revert plugins system

--- apps/plugins/server/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import {app as getApp} from '@greenpress/api-kit'
-import {managePlugins} from './manage-plugins';
-import {playPlugins} from './play-plugins';
-
-export async function loadRoutes() {
-  const app = getApp()
-  app.use(managePlugins());
-  app.use(await playPlugins());
-}
