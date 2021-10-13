72bfc1f90288c7df3e7485b49cc510805c2c9aa5
David Meir-Levy <davidmeirlevy@gmail.com>
2021-10-13 17:03:08 +0300

    added mongoose models

---
+++ apps/plugins/server/models/index.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+export const connect = (uri: string) => {
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  // plug in the promise library:
+  mongoose.Promise = global.Promise
+
+  mongoose.connection.on('error', (err) => {
+    console.error(`Mongoose connection error: ${err}`)
+    process.exit(1)
+  })
+
+  // load models
+  require('./plugin')
+}
