f03f4526d2a42f5240f5a250f0f9af13ed8d38df
David Meir-Levy <davidmeirlevy@gmail.com>
2021-10-13 18:18:00 +0300

    added routes

--- apps/plugins/index.ts
+++ apps/plugins/index.ts
@@ -1,13 +1,11 @@
 import {start} from '@greenpress/api-kit';
 import {connect} from './server/models';
 import {mongoUri} from './config';
-// import {loadRoutes} from './server/routes';
+import {loadRoutes} from './server/routes';
 //
 // // connect to the database and load models
 connect(mongoUri)
-//
-// loadRoutes().then(() => {
-//   start('Plugins Service', process.env.PORT || 9006, process.env.IP || '127.0.0.1')
-// })
 
-start('Plugins Service', process.env.PORT || 9006, process.env.IP || '127.0.0.1')
+loadRoutes().then(() => {
+  start('Plugins Service', process.env.PORT || 9006, process.env.IP || '127.0.0.1')
+})
---
+++ apps/plugins/server/routes/play-plugins.ts
@@ -0,0 +1,22 @@
+import {getRouter} from '@greenpress/api-kit/router';
+// import {getPlugins} from '../controllers/play-plugins';
+// import {createProxyMiddleware} from 'http-proxy-middleware';
+// import {proxyApiPrefix} from '../../config';
+
+
+export async function playPlugins() {
+ // const plugins = await getPlugins();
+  const router = getRouter();
+  //
+  // plugins.forEach(plugin => {
+  //   router.use(proxyApiPrefix + '/' + plugin.apiPath, createProxyMiddleware({
+  //     target: plugin.proxyUrl,
+  //     changeOrigin: true,
+  //     headers: {
+  //       token: plugin.token,
+  //     }
+  //   }))
+  // });
+  //
+  // return router;
+}
