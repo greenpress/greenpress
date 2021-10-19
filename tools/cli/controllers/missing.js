b2f085a6c5137051ae1684055027d34ee6269ce8
Meer Hamza <60890818+MeerHamza1421@users.noreply.github.com>
2021-10-18 23:37:01 -0700

    🐛 FIX: missing command issue (#122)

--- tools/cli/controllers/missing.js
+++ tools/cli/controllers/missing.js
@@ -1,11 +1,12 @@
 const { checkDependencyVersion } = require('../services/missing');
 
 const dependencies = [
-	[ 'git', 'https://git-scm.com/downloads' ],
-	[ 'docker', 'https://www.docker.com/products/docker-desktop' ],
-	[ 'docker-compose', 'https://www.docker.com/products/docker-desktop' ],
-	[ 'node', 'https://nodejs.org/en/download/' ] ];
-	[ 'heroku', 'https://devcenter.heroku.com/articles/heroku-cli‏' ]
+	['git', 'https://git-scm.com/downloads'],
+	['docker', 'https://www.docker.com/products/docker-desktop'],
+	['docker-compose', 'https://www.docker.com/products/docker-desktop'],
+	['node', 'https://nodejs.org/en/download/'],
+	['heroku', 'https://devcenter.heroku.com/articles/heroku-cli‏']
+];
 module.exports = function missingController() {
-	dependencies.forEach(([ app, installLink ]) => checkDependencyVersion(app, installLink));
+	dependencies.forEach(([app, installLink]) => checkDependencyVersion(app, installLink));
 }
