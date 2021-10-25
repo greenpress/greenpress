3d9c00d774ead5f25dfad208054450f9d62b4fb3
David Meir-Levy <davidmeirlevy@gmail.com>
2021-10-25 13:17:25 +0300

    fix create user component

--- apps/admin/src/modules/users/CreateUser.vue
+++ apps/admin/src/modules/users/CreateUser.vue
@@ -5,23 +5,24 @@
 	</div>
 </template>
 <script>
-  import { useCreateUser } from './compositions/users'
-  import UserForm from './components/UserForm.vue'
-  import PageTitle from '../core/components/semantics/PageTitle.vue'
+import { useRouter } from 'vue-router';
+import { useCreateUser } from './compositions/users'
+import UserForm from './components/UserForm.vue'
+import PageTitle from '../core/components/semantics/PageTitle.vue'
 
-  export default {
-    name: 'CreateUser',
-    components: { PageTitle, UserForm },
-    setup() {
-			const router = useRouter()
+export default {
+	name: 'CreateUser',
+	components: { PageTitle, UserForm },
+	setup() {
+		const router = useRouter()
 
-			const { createUser } = useCreateUser()
-      return {
-        save: async user => {
-          const { _id } = await createUser(user)
-					router.push({ name: 'editUser', params: { userId: _id } })
-        }
-      }
-    }
-  }
+		const { createUser } = useCreateUser()
+		return {
+			save: async user => {
+				const { _id } = await createUser(user)
+				await router.push({ name: 'editUser', params: { userId: _id } })
+			}
+		}
+	}
+}
 </script>
