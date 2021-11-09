3ae60e209f1fa0ebaceabff2bcb8558708e6e03c
David Meir-Levy <davidmeirlevy@gmail.com>
2021-11-09 16:57:48 +0200

    app config sdk

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -3,17 +3,20 @@ import BaseSDK from './base-gp-sdk';
 import GpCategories from './categories';
 import GpPosts from './posts';
 import GpMenus from './menus';
+import GpConfigurations from './configurations';
 
 export default class GreenpressSDK extends BaseSDK {
 
   categories: GpCategories;
   posts: GpPosts;
   menus: GpMenus;
+  configurations: GpConfigurations;
 
   constructor(private options: GreenpressSDKOptions) {
     super(options);
     this.categories = new GpCategories(this.options);
     this.posts = new GpPosts(this.options);
     this.menus = new GpMenus(this.options);
+    this.configurations = new GpConfigurations(this.options);
   }
 }
