0a329016d6c9576dd16bab96de69db38e5460ccd
David Meir-Levy <davidmeirlevy@gmail.com>
2021-11-12 13:57:48 +0200

    new ssr service, using vite + fastify + vue3

---
+++ apps/front-ssr/views/post.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>post</h1>
+</template>
+
+<script lang="ts">
+export const path = '/:category/:post'
+
+export default {
+  setup() {
+    return {
+      layout: []
+    }
+  }
+}
+</script>
---
+++ apps/front-ssr/views/search.vue
@@ -0,0 +1,20 @@
+<template>
+  <Layout :layout="layout"/>
+</template>
+
+<script lang="ts">
+import Layout from '../src/components/Layout.vue';
+
+export const path = '/search'
+
+export default {
+  components: {Layout},
+  setup() {
+    return {
+      layout: [
+        {component: 'SearchForm', predefined: true},
+      ]
+    }
+  }
+}
+</script>
