f80c40ddaaea10957450b2572ed83e088e228489
David Meir-Levy <davidmeirlevy@gmail.com>
2021-11-12 14:32:04 +0200

    build logs

--- apps/front-ssr/build.mjs
+++ apps/front-ssr/build.mjs
@@ -1,11 +1,14 @@
 import {execSync} from 'child_process'
 
 try {
+  console.log('running SSR build');
   const result = execSync('node app.mjs build').toString().trim();
-  console.log(result);
+  console.log('build result: ', result);
   if(result !== 'Generated dist/client and dist/server.') {
     process.exit(1);
   }
-} catch {
+} catch (err) {
+  console.log('failed to build SSR');
+  console.log(err.message);
   process.exit(1);
 }
