236d3e61d81f2b89ddea9f976457ca66999f2a49
David Meir-Levy <davidmeirlevy@gmail.com>
2021-11-13 19:54:47 +0200

    start script for front-ssr

--- apps/front-ssr/build.mjs
+++ apps/front-ssr/build.mjs
@@ -1,10 +1,10 @@
-import {execSync} from 'child_process'
+import { execSync } from 'child_process'
 
 try {
   console.log('running SSR build');
   const result = execSync('node app.mjs build').toString().trim();
   console.log('build result: ', result);
-  if(result !== 'Generated dist/client and dist/server.') {
+  if (result !== 'Generated dist/client and dist/server.') {
     process.exit(1);
   }
 } catch (err) {
