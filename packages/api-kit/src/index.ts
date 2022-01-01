0cbce8bb7f7075d630b5c61946aa0066239a4d8f
Naor Peled <thehecticbyte@gmail.com>
2022-01-01 12:46:26 +0200

    revert

--- packages/api-kit/src/index.ts
+++ packages/api-kit/src/index.ts
@@ -1,9 +1,9 @@
 import express, { Express } from 'express';
 import type { ApiConfig, BodyParserType } from './types';
 
-let app: Express;
+let _app: Express;
 
-let config: ApiConfig = {
+let _config: ApiConfig = {
   cors: !!process.env.API_CORS || false,
   bodyParser: (process.env.API_BODY_PARSER as BodyParserType) || 'json',
   port: parseInt(process.env?.PORT) || 3000,
@@ -11,39 +11,37 @@ let config: ApiConfig = {
 };
 
 function createApp() {
-  app = express()
+  _app = express()
 
   if (process.env.NODE_ENV !== 'production') {
-    app.use(require('morgan')('combined'))
+    _app.use(require('morgan')('combined'))
   }
-  if (config.cors) {
-    app.use(require('cors')())
+  if (_config.cors) {
+    _app.use(require('cors')())
   }
-  if (config.bodyParser) {
-    app.use(express[config.bodyParser]())
+  if (_config.bodyParser) {
+    _app.use(express[_config.bodyParser]())
   }
 
-  return app;
+  return _app;
 }
 
-function startApp(serviceName = 'APP', port = config.port, ip = config.ip): Promise<void> {
-  app.set('port', port)
-  app.set('ip', ip)
+function startApp(serviceName = 'APP', port = _config.port, ip = _config.ip): Promise<void> {
+  _app.set('_port', port)
+  _app.set('_ip', ip)
 
   // start the server
   return new Promise((resolve) => {
-    app.listen(port, ip, () => {
+    _app.listen(port, ip, () => {
       console.log(`${serviceName} is running on port ${port}`);
       resolve();
     });
   })
 }
 
-module.exports = {
-  config: (updatedConfig = config): ApiConfig => {
-    config = { ...config, ...updatedConfig };
-    return config;
-  },
-  app: () => app || createApp(),
-  start: startApp
+export const app = () => _app || createApp();
+export const config = (updatedConfig = config): ApiConfig => {
+  _config = { ..._config, ...updatedConfig };
+  return config;
 }
+export const start = startApp;
