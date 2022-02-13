43657750c8fd97af88aa1a5ced6418af60707024
David Meir-Levy <davidmeirlevy@gmail.com>
2022-02-13 11:08:00 +0200

    workaround typo

--- apps/front-ssr/build-workaround.mjs
+++ apps/front-ssr/build-workaround.mjs
@@ -4,7 +4,7 @@ import { readFile, writeFile } from 'fs/promises'
 try {
   const lib = await readFile('./node_modules/fastify-vite/index.mjs');
   await writeFile('./node_modules/fastify-vite/index.mjs',
-    lib.toString().replaceAll('process.exit()', 'process.exit(1)')
+    lib.toString().replaceAll('process.exit()', 'process.exit(0)')
   );
 } catch {
   console.log('fastify-vite folder not found')
