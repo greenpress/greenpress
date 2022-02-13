d3b3f8278a22bebf5796dfb51d10b8665e037a4a
David Meir-Levy <davidmeirlevy@gmail.com>
2022-02-13 11:13:24 +0200

    added workaround message

--- apps/front-ssr/build-workaround.mjs
+++ apps/front-ssr/build-workaround.mjs
@@ -2,10 +2,11 @@ import { readFile, writeFile } from 'fs/promises'
 
 
 try {
-  const lib = await readFile('./node_modules/fastify-vite/index.mjs');
-  await writeFile('./node_modules/fastify-vite/index.mjs',
+  const lib = await readFile('./node_modules/fastify-vite/index.js');
+  await writeFile('./node_modules/fastify-vite/index.js',
     lib.toString().replaceAll('process.exit()', 'process.exit(0)')
   );
+  console.log('fastify-vite workaround changed')
 } catch {
   console.log('fastify-vite folder not found')
 }
