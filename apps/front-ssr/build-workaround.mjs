922fb18ef8f8ddd2fb4ec8e325d39fcf0e2c3e5e
David Meir-Levy <davidmeirlevy@gmail.com>
2022-02-13 11:03:15 +0200

    added workaround for exit zero

---
+++ apps/front-ssr/build-workaround.mjs
@@ -0,0 +1,11 @@
+import { readFile, writeFile } from 'fs/promises'
+
+
+try {
+  const lib = await readFile('./node_modules/fastify-vite/index.mjs');
+  await writeFile('./node_modules/fastify-vite/index.mjs',
+    lib.toString().replaceAll('process.exit()', 'process.exit(1)')
+  );
+} catch {
+  console.log('fastify-vite folder not found')
+}
