38e814df1407f4340977b00a33ec865eb29c05df
David Meir-Levy <davidmeirlevy@gmail.com>
2022-02-16 13:40:46 +0200

    use dynamic tenant

--- apps/gateway/index.ts
+++ apps/gateway/index.ts
@@ -1,9 +1,10 @@
 import { start, config, app as getApp } from "@greenpress/api-kit";
 import cacheManager from "./cache-manager";
+import apiProxy from "@greenpress/api-proxy-middleware";
 
 config({ cors: false, bodyParser: false });
 
 const app = getApp();
-require("@greenpress/api-proxy-middleware")(app, {}, cacheManager);
+apiProxy(app, {}, cacheManager);
 
 start("Gateway", process.env.PORT || 3000, process.env.IP || "0.0.0.0");
