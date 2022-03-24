f0e9880f8bee92f8101d25c4296149a64d6c0139
ChaniGintzler <33652096+ChaniGintzler@users.noreply.github.com>
2022-03-24 15:16:55 +0200

    add save cookie token in redis (#153)

---
+++ apps/auth/server/utils/cache-manager.ts
@@ -0,0 +1,12 @@
+const { redisUrl } = require('../../config')
+
+const { createCacheManager } = require('@greenpress/cache-manager');
+
+const cacher = redisUrl ?
+  require('@greenpress/cache-manager/dist/redis-cache').createRedisCache(redisUrl) :
+  require('@greenpress/cache-manager/dist/memory-cache').createMemoryCache()
+
+const cacheManager = createCacheManager(cacher);
+
+export  {cacheManager};
+  
