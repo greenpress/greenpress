884d60bde09cc39e4af70ae13de9b7fea401ea6f
David Meir-Levy <davidmeirlevy@gmail.com>
2022-10-06 11:50:11 +0300

    added tests for admin panel

--- apps/admin/src/plugins/element.ts
+++ apps/admin/src/plugins/element.ts
@@ -1,11 +1,11 @@
-import ElementPlus from "element-plus";
-import * as icons from "@element-plus/icons-vue";
-import "element-plus/dist/index.css";
+import ElementPlus from 'element-plus';
+import * as icons from '@element-plus/icons-vue';
+import 'element-plus/dist/index.css';
 
 export default function (app) {
   app.use(ElementPlus);
 
   Object.keys(icons).forEach((name) => {
-    app.component("Icon" + name, icons[name]);
+    app.component('Icon' + name, icons[name]);
   });
 }
